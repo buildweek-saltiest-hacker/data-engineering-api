afd18d8205837a4b60b37032a527c9970193a15f
tigju <tigju24@gmail.com>
2020-06-17 23:34:51 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index.html")
+def index():
+    return "Hello from Flask!!!"
+
+
+if __name__ == '__main__':
+    app.run()
