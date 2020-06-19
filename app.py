435169d6426acfd507514ef1e24d48659cddfb6d
tigju <tigju24@gmail.com>
2020-06-18 20:52:16 -0400

    Setting up heroku

--- app.py
+++ app.py
@@ -7,18 +7,18 @@ app = Flask(__name__)
 @app.route("/index.html")
 def index():
     return "Hello from Flask!!!"
-
-@app.route("/comments")
-def comments():
-    return "Comments"
-
-@app.route("/rankings")
-def rankings():
-    return "Rankings"
-
-@app.route("/negativity-score")
-def negativity_score():
-    return "Negativity Score"
+### samples
+# @app.route("/comments")
+# def comments():
+#     return "Comments"
+#
+# @app.route("/rankings")
+# def rankings():
+#     return "Rankings"
+#
+# @app.route("/negativity-score")
+# def negativity_score():
+#     return "Negativity Score"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
