b40cfa70d03a621ad76ecd773ec38e2e009d37e6
BrokenShell <webmaster@sharpdesigndigital.com>
2020-06-20 22:03:52 -0700

    adds /score-by-author & /comment-by-id

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ from flask import Flask, make_response, request, jsonify
 from models import db, migrate, Comment, parse_records
 import os
 
-# from dotenv import load_dotenv
-# load_dotenv()
+from dotenv import load_dotenv
 
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 app = Flask(__name__)
@@ -22,19 +22,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 
 @app.route('/')
 def documentation():
-    return jsonify({
-        'Project': 'Saltiest Hacker API',
-        'Developers': [
-            'Iuliia Stanina',
-            'Robert Sharp',
-        ],
-        'Planned End Points': [
-            '/',
-            '/score-by-author/<author>',
-            '/comment-by-id/<comment_id>',
-            '/comments-by-author/<author>',
-        ]
-    })
+    return jsonify((
+        'Project: The Saltiest Hacker API',
+        'Developers: Iuliia Stanina & Robert Sharp',
+    ))
 
 
 @app.route('/test')
@@ -45,22 +36,46 @@ def test():
 
 
 @app.route('/comments-by-author/<author>')
-def get_author(author=None):
-    author_comments = parse_records(Comment.query.filter_by(author=author).all())
-    comments_list = {'comments': []}
-    for c in author_comments:
-        comments_list['comments'].append({
-            'id': c['id'],
-            'comment': c['comment'],
-            'saltiness': c['saltiness'],
-        })
-    result = sorted(comments_list['comments'], key=lambda x: x['saltiness'], reverse=True)
-    return jsonify(result[:10])
+def comments_by_author(author):
+    """ Returns list of the top 3 most salty comments for any author
+        http://127.0.0.1:5000/comments-by-author/po """
+    comments = Comment.query.filter_by(author=author)
+    parsed = parse_records(comments.order_by(Comment.saltiness).all())
+    ordered = [{
+        'id': item['id'],
+        'comment': item['comment'],
+        'saltiness': item['saltiness'],
+    } for item in reversed(parsed[-3:])]
+    return jsonify(ordered)
+
+
+@app.route('/score-by-author/<author>')
+def score_by_author(author):
+    """ Returns the average saltiness of all comments by this author
+    http://127.0.0.1:5000/score-by-author/po """
+    return jsonify(Comment.query.filter_by(author=author).first().score)
+
+
+@app.route('/comment-by-id/<comment_id>')
+def comment_by_id(comment_id):
+    """ Comment text by comment_id
+    http://127.0.0.1:5000/comment-by-id/42 """
+    return jsonify(Comment.query.filter_by(id=comment_id).first().comment)
+
+
+# @app.route('/top-hackers/<number>')
+# def top_hackers(number=3):
+#     hackers = parse_records(Hacker.query.limit(number).all())
+#     return jsonify([{
+#         'rank': hacker.rank,
+#         'name': hacker.name,
+#         'score': hacker.score,
+#     } for hacker in hackers])
 
 
 @app.before_request
 def before_request():
-    """ CORS preflight """
+    """ CORS preflight, required for off-server access """
 
     def _build_cors_prelight_response():
         response = make_response()
@@ -75,7 +90,7 @@ def before_request():
 
 @app.after_request
 def after_request(response):
-    """ CORS headers """
+    """ CORS headers, required for off-server access """
     header = response.headers
     header['Access-Control-Allow-Origin'] = '*'
     return response
--- models.py
+++ models.py
@@ -22,6 +22,15 @@ class Comment(db.Model):
         return f'<Comment {self.id} {self.author} {self.comment} {self.saltiness} {self.score}'
 
 
+# class Hacker(db.Model):
+#     rank = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(128))
+#     score = db.Column(db.Integer)
+#
+#     def __repr__(self):
+#         return f'<Comment {self.rank} {self.name} {self.score}'
+
+
 def parse_records(database_records):
     parsed_records = []
     for record in database_records:
